4183c4e3dfc8f7331b89a745b88096c2105b9be3
dalya kablawi <dalyakablawi@gmail.com>
2025-03-13 21:29:52 -0400

    completed

---
+++ my-portfolio/src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <ul>
+        <li>
+          <a href="https://github.com/your-github">GitHub</a>
+        </li>
+        <li>
+          <a href="https://linkedin.com/in/your-linkedin">LinkedIn</a>
+        </li>
+        <li>
+          <a href="https://twitter.com/your-twitter">Twitter</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
+
+export default Footer;
